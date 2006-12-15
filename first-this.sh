36db25e254877d00f5cd64742626e502d7dccde9
eloy <>
2006-12-15 12:45:21 +0000

    *** empty log message ***

--- first-this.sh
+++ first-this.sh
@@ -6,8 +6,8 @@ echo "autoheader"
 autoheader
 echo "autoconf"
 autoconf
-echo "automake -a -c"
-automake -a -c
 echo "libtoolize --automake -c"
 libtoolize --automake -c
+echo "automake -a -c"
+automake -a -c
 
