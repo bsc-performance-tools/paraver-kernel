748238348004ddce5cdd6e0bcb713d6582cf2094
eloy <>
2007-03-01 12:34:31 +0000

    *** empty log message ***

---
+++ include/processmodel.h
@@ -0,0 +1,20 @@
+#ifndef PROCESSMODEL_H_INCLUDED
+#define PROCESSMODEL_H_INCLUDED
+
+class ProcessModel
+{
+
+  public:
+    ProcessModel();
+    ~ProcessModel();
+
+  protected:
+
+  private:
+
+};
+
+
+
+
+#endif // PROCESSMODEL_H_INCLUDED
---
+++ src/processmodel.cpp
@@ -0,0 +1,3 @@
+
+#include "processmodel.h"
+
