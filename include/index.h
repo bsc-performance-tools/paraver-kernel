d1b0fb23c55beb55fa137b1f4e86443f3d1c9b38
pedro <>
2007-08-01 13:23:19 +0000

    Ahora si.

---
+++ include/index.h
@@ -0,0 +1,30 @@
+#ifndef INDEX_H_INCLUDED
+#define INDEX_H_INCLUDED
+
+#include "paraverkerneltypes.h"
+#include "bplustreetypes.h"
+
+#include <map>
+
+using std::map;
+using namespace bplustree;
+
+typedef map< TRecordTime, TRecord * > TTraceIndex;
+
+static UINT32 INDEX_STEP = 10000;
+
+class Index
+{
+  public:
+    Index( UINT32 step = INDEX_STEP );
+    ~Index();
+
+    void indexRecord( TRecord *rec );
+    TRecord *findRecord( TRecordTime time );
+
+  private:
+    UINT32 indexStep;
+    TTraceIndex baseIndex;
+};
+
+#endif // INDEX_H_INCLUDED
