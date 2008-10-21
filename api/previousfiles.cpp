2c3deef23878373266d9f8fdbdbb0704c5ca27ba
pedro <>
2008-10-21 13:18:38 +0000

    *** empty log message ***

---
+++ api/previousfiles.cpp
@@ -0,0 +1,72 @@
+#include "previousfiles.h"
+
+PreviousFiles::PreviousFiles( const string &filename ): myFileName( filename )
+{
+  fstream myFile;
+
+  myFile.open( myFileName.c_str(), ios::in );
+  if ( !myFile.fail() )
+    throw exception();
+  else
+  {
+    read( myFile );
+    myFile.close();
+  }
+}
+
+
+PreviousFiles::~PreviousFiles()
+{
+  if ( myFile.is_open() )
+    myFile.close();
+}
+
+
+void PreviousFiles::read( fstream &myFile )
+{
+  UINT16 lines = 0;
+
+  while ( !myFile.eof() && lines < SIZE )
+  {
+    string strLine;
+
+    getline( myFile, strLine );
+    listFiles.insert( strLine );
+
+    lines ++;
+  }
+}
+
+
+bool PreviousFiles::update( )
+{
+  fstream myFile;
+
+  myFile.open(  myFileName.c_str(), ios::out );
+  if ( !myFile.fail() )
+    return false;
+
+  for ( set<string>::iterator it = listFiles.begin(); it != listFiles.end(); it++ )
+    myFile << *it << endl;
+
+  myFile.close();
+
+  return true;
+}
+
+
+bool PreviousFiles::add( string newFile )
+{
+  set<string>::iterator it = listFiles.find( newFile );
+
+  if ( it != listFiles.end() )
+      listFiles.erase( it );
+
+  listFiles.insert( listFiles.begin(), newFile );
+
+  if ( listFiles.size() > SIZE )
+    listFiles.erase( --listFiles.end() );
+
+  return update();
+}
+
---
+++ api/previousfiles.h
@@ -0,0 +1,32 @@
+#ifndef PREVIOUSFILES_H_INCLUDED
+#define PREVIOUSFILES_H_INCLUDED
+
+#include <string>
+#include <fstream>
+using namespace std;
+#include <set>
+
+#include "paraverkerneltypes.h"
+
+class PreviousFiles
+{
+  public:
+    PreviousFiles( const string &filename );
+    ~PreviousFiles();
+
+    bool add( string newFile );
+
+  private:
+    static const UINT16 SIZE = 20;
+
+    fstream myFile;
+    string  myFileName;
+    set < string > listFiles;
+
+    void create();
+    bool update();
+    void read( fstream &myFile );
+};
+
+
+#endif // PREVIOUSFILES_H_INCLUDED
