6ecfd4c54fd3ae4fb4ab22e2eca56bb524e19a76
eloy <>
2010-06-16 10:37:31 +0000

    *** empty log message ***

--- api/semanticcolor.cpp
+++ api/semanticcolor.cpp
@@ -294,9 +294,13 @@ rgb GradientColor::calcColor( TSemanticValue whichValue,
                               TSemanticValue minimum,
                               TSemanticValue maximum ) const
 {
-  if ( whichValue == 0 && !drawOutOfScale &&
-      ParaverConfig::getInstance()->getColorsTimelineUseZero() )
-    return ParaverConfig::getInstance()->getColorsTimelineColorZero();
+  if ( whichValue == 0 && !drawOutOfScale )
+  {
+    if( ParaverConfig::getInstance()->getColorsTimelineUseZero() )
+      return ParaverConfig::getInstance()->getColorsTimelineColorZero();
+    else
+      return SemanticColor::BACKGROUND;
+  }
 
   if ( whichValue < minimum )
   {
