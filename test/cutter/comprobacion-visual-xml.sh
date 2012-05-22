31569391578e1d040ee195d2f0cb970b9ef2abc9
pedro <>
2012-05-22 14:26:10 +0000

    *** empty log message ***

--- test/cutter/comprobacion-visual-xml.sh
+++ test/cutter/comprobacion-visual-xml.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for f in `ls -a1 *.xml`; do clear; cat $f; echo "Press return"; read $tocatecla; done
+for f in `ls -a1 *.xml`; do clear; cat $f; echo; echo; echo "Press return"; read $tocatecla; done
