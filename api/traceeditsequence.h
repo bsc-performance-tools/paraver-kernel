2a2af5a13142e89a3026b5cd8a994659ba919733
eloy <>
2013-08-12 09:00:03 +0000

    *** empty log message ***

---
+++ api/traceeditsequence.h
@@ -0,0 +1,40 @@
+#ifndef TRACEEDITSEQUENCE_H_INCLUDED
+#define TRACEEDITSEQUENCE_H_INCLUDED
+
+class TraceEditState
+{
+public:
+  TraceEditState();
+  ~TraceEditState();
+protected:
+
+private:
+
+};
+
+
+class TraceEditAction
+{
+public:
+  TraceEditAction();
+  ~TraceEditAction();
+protected:
+
+private:
+
+};
+
+
+class TraceEditSequence
+{
+public:
+  TraceEditSequence();
+  ~TraceEditSequence();
+protected:
+
+private:
+
+};
+
+
+#endif // TRACEEDITSEQUENCE_H_INCLUDED
