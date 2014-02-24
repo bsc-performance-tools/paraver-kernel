930270d6b93d3cc1ea525dcf377f58421b407529
eloy <>
2014-02-24 14:39:31 +0000

    *** empty log message ***

--- src/noloadblocks.cpp
+++ src/noloadblocks.cpp
@@ -451,12 +451,16 @@ void NoLoadBlocks::getNextRecord( TRecord **record, PRV_INT64& offset, PRV_UINT1
   else
     offset = initialOffset;
 
-  while ( blocks.count( offset ) == 0 )
+  if ( blocks.count( offset ) == 0 )
   {
     file->clear();
     file->seekg( offset );
     lastData = NULL;
     lastPos = offset;
+  }
+
+  while ( blocks.count( offset ) == 0 )
+  {
     body->read( file, *this, notUsedEvents, dummyTraceInfo );
 
     if( file->tellg() == endFileOffset )
