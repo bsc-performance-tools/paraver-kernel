fb38ac01f8e263ca17b810311b7afea830d42bcc
pedro <>
2016-06-06 13:01:30 +0000

    *** empty log message ***

--- first-this.sh
+++ first-this.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-echo "aclocal"
+set -x
+
 aclocal -I m4
-echo "autoheader"
 autoheader
-echo "autoconf"
 autoconf
-echo "libtoolize --automake -c"
 case `uname` in Darwin*) glibtoolize --automake -c ;;
   *) libtoolize --automake -c ;; esac
-echo "automake -a -c"
 automake -a -c
 
