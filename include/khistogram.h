b48838aa7b99560698034012537174b90bf6492b
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-05-09 19:26:15 +0200

    Adding OpenMP parallelization
    Use inline tasks instead of outline tasks.
    Since this is an OpenMP parallelization, we have to open a parallel region too.

--- include/khistogram.h
+++ include/khistogram.h
@@ -315,7 +315,6 @@ class KHistogram : public Histogram
                             TObjectOrder fromRow, TObjectOrder toRow,
                             std::vector<TObjectOrder>& selectedRows  );
 
-    #pragma omp task
     void executionTask( TRecordTime fromTime, TRecordTime toTime,
                         TObjectOrder fromRow, TObjectOrder toRow,
                         std::vector<TObjectOrder>& selectedRows );
