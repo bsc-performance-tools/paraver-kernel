5dd291e600742aad9e842c89cce1d92e502c8d9e
Eloy Martinez <eloy.martinez@bsc.es>
2018-01-25 13:35:07 +0100

    Added two options to paramedir about timeline csv:   - Option "-obj" to show objects hierarchy instead of global order   - Option "-tu" to use trace time units instead of window units

--- api/paramedir.cpp
+++ api/paramedir.cpp
@@ -89,6 +89,10 @@ enum TOptionID
   MANY_FILES,
   OUTPUT_NAME,
 
+  // TIMELINES
+  TIMELINE_OBJECT_HIERARCHY,
+  TIMELINE_TRACE_UNITS,
+
   // HISTOGRAMS
   EMPTY_COLUMNS,
   PRINT_PLANE,
@@ -126,6 +130,10 @@ TOptionParamedir definedOption[] =
   // FILES
   { "-m", "--many-files", false, 0, "", "", "Allows to separate cfg output (default in a unique file)" },
   { "-o", "--output-name", false, 1, "", "<tracename>",  "Output trace name (only for processing trace)" },
+
+  // TIMELINES
+  { "-obj", "--object-hierarchy", false, 0, "", "", "Print object hierarchy in timelines instead of global object order" },
+  { "-tu", "--trace-units", false, 0, "", "",  "Use trace time units in timelines instead of time window units selected in the cfg file" },
 
   // HISTOGRAMS
   { "-e", "--empty-columns", false, 0, "", "", "Hide empty columns" },
@@ -298,7 +306,13 @@ void printHelp()
   std::cout << std::endl;
   std::cout << "  Output file options:" << std::endl;
 
-  for ( int i = MANY_FILES; i < EMPTY_COLUMNS; ++i )
+  for ( int i = MANY_FILES; i < TIMELINE_OBJECT_HIERARCHY; ++i )
+    printOptionHelp( TOptionID( i ) );
+
+  std::cout << std::endl;
+  std::cout << "  Timeline options:" << std::endl;
+
+  for ( int i = TIMELINE_OBJECT_HIERARCHY; i < EMPTY_COLUMNS; ++i )
     printOptionHelp( TOptionID( i ) );
 
   std::cout << std::endl;
@@ -813,6 +827,8 @@ void loadCFGs( KernelConnection *myKernel )
     {
       TextOutput output;
       output.setMultipleFiles( option[ MANY_FILES ].active );
+      output.setObjectHierarchy( option[ TIMELINE_OBJECT_HIERARCHY ].active );
+      output.setWindowTimeUnits( !option[ TIMELINE_TRACE_UNITS ].active );
 
       if ( histograms.begin() != histograms.end() &&
            histograms[ histograms.size() - 1 ] != NULL )
