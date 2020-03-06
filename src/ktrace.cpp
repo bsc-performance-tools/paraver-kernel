36c1025d25f59415b0e0065ab85d16495bccf2e6
MNXANL <miguel.moreno@bsc.es>
2020-03-06 15:46:03 +0100

    Read CSV successful ; setEventTypePrecision now uses the insert function to use the first precision and prevent overriding

--- src/ktrace.cpp
+++ src/ktrace.cpp
@@ -1022,13 +1022,7 @@ void KTrace::setPhysicalReceive( TCommID whichComm, TRecordTime whichTime )
 
 void KTrace::setEventTypePrecision( TEventType whichType, double whichPrecision )
 {
-  /*
-  if ( eventsPrecision.find( whichType ) == eventsPrecision.end() ||
-     ( eventsPrecision.find( whichType ) == eventsPrecision.end() &&
-       whichPrecision > eventsPrecision[ whichType ] ) )
-    //eventsPrecision[ whichType ] = whichPrecision;
-  */
-    eventsPrecision.insert( { whichType, whichPrecision } ) ;
+  eventsPrecision.insert( { whichType, whichPrecision } ) ;
 }
 
 double KTrace::getEventTypePrecision( TEventType whichType ) const
