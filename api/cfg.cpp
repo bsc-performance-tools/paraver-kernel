340c0f2af041765901297a8c5e4a855ecce08770
Pedro Antonio Gonzalez Navarro <pedro.gonzalez@bsc.es>
2025-04-07 11:06:42 +0200

    Fixed loop to properly save derived histogram parents in cfg.

--- api/cfg.cpp
+++ api/cfg.cpp
@@ -758,7 +758,7 @@ void CFGLoader::pushbackHistogram( Histogram *whichHistogram, vector<Histogram *
 {
   if ( whichHistogram->isDerivedHistogram() )
   {
-    for ( PRV_UINT16 i = 1; i > 0; --i )
+    for ( const PRV_UINT16 i: { 1, 0 }  )
       pushbackHistogram( whichHistogram->getParent( i ), allHistograms );
   }
 
