ab03315ab0855dcffd3779f401696c91d52f7f99
Eloy Martinez <eloy.martinez@bsc.es>
2025-08-13 12:00:36 +0200

    Removed unsupported register specifier in C++17

--- src/tracebodyio_csv.cpp
+++ src/tracebodyio_csv.cpp
@@ -21,10 +21,11 @@
  *   Barcelona Supercomputing Center - Centro Nacional de Supercomputacion   *
 \*****************************************************************************/
 
-#include <string>
-#include <sstream>
-#include <iostream>
 #include "tracebodyio_csv.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -35,13 +36,13 @@ string TraceBodyIO_csv::line;
 ostringstream TraceBodyIO_csv::ostr;
 
 // Optimization on conversion string to numbers, but with no error control
-//#define USE_ATOLL
+// #define USE_ATOLL
 // Even more optimization using custom function instead of atoll with error checking
 #define USE_PRV_ATOLL
-template <typename T>
-bool prv_atoll( const char *p, T *result )
+template< typename T >
+bool prv_atoll( const char* p, T* result )
 {
-  register long long int tmp = 0;
+  long long int tmp  = 0;
   long long int sign = 1;
 
   if( *p == '-' )
@@ -62,11 +63,11 @@ bool prv_atoll( const char *p, T *result )
   return true;
 }
 
-template <typename T>
-bool prv_atoll( const char *p, T *result, double *decimals )
+template< typename T >
+bool prv_atoll( const char* p, T* result, double* decimals )
 {
-  register long long int tmp = 0;
-  double decs = 1.0;
+  long long int tmp  = 0;
+  double decs        = 1.0;
   long long int sign = 1;
 
   if( *p == '-' )
@@ -77,7 +78,7 @@ bool prv_atoll( const char *p, T *result, double *decimals )
   bool dotFound = false;
   while( ( '0' <= *p && *p <= '9' ) || *p == '.' )
   {
-    if ( *p == '.' )
+    if( *p == '.' )
     {
       ++p;
       break;
@@ -90,7 +91,7 @@ bool prv_atoll( const char *p, T *result, double *decimals )
   }
   while( '0' <= *p && *p <= '9' )
   {
-    tmp = ( tmp * 10 ) + ( *p - '0' );
+    tmp  = ( tmp * 10 ) + ( *p - '0' );
     decs = decs / 10.0f;
     ++p;
   }
@@ -98,14 +99,14 @@ bool prv_atoll( const char *p, T *result, double *decimals )
   if( *p != '\n' && *p != '\r' && *p != '\0' )
     return false;
 
-  *result = tmp * sign;
+  *result   = tmp * sign;
   *decimals = decs;
   return true;
 }
 
-TraceBodyIO_csv::TraceBodyIO_csv( Trace* trace, ProcessModel<>& whichProcessModel )
-: myTrace( trace ),  myProcessModel( &whichProcessModel )
-{}
+TraceBodyIO_csv::TraceBodyIO_csv( Trace* trace, ProcessModel<>& whichProcessModel ) : myTrace( trace ), myProcessModel( &whichProcessModel )
+{
+}
 
 
 bool TraceBodyIO_csv::ordered() const
@@ -117,13 +118,13 @@ void TraceBodyIO_csv::read( TraceStream& file,
                             MemoryBlocks& records,
                             const ProcessModel<>& whichProcessModel,
                             const ResourceModel<>& whichResourceModel,
-                            std::unordered_set<TState>& states,
-                            std::unordered_set<TEventType>& events,
+                            std::unordered_set< TState >& states,
+                            std::unordered_set< TEventType >& events,
                             MetadataManager& traceInfo,
                             TRecordTime& endTime ) const
 {
   file.getline( line );
-  if ( line.size() == 0 )
+  if( line.size() == 0 )
     return;
 
   readEvents( whichResourceModel, line, records, states, endTime );
@@ -131,10 +132,10 @@ void TraceBodyIO_csv::read( TraceStream& file,
 
 void TraceBodyIO_csv::bufferWrite( fstream& whichStream, bool writeReady, bool lineClear ) const
 {
-  if ( writeReady )
+  if( writeReady )
   {
     whichStream << line << '\n';
-    if ( lineClear )
+    if( lineClear )
       line.clear();
   }
 }
@@ -143,7 +144,7 @@ void TraceBodyIO_csv::bufferWrite( fstream& whichStream, bool writeReady, bool l
 void TraceBodyIO_csv::write( std::fstream& whichStream,
                              const ProcessModel<>& whichProcessModel,
                              const ResourceModel<>& whichResourceModel,
-                             MemoryTrace::iterator *record ) const
+                             MemoryTrace::iterator* record ) const
 {
 }
 
@@ -151,7 +152,7 @@ void TraceBodyIO_csv::write( std::fstream& whichStream,
 /**********************
   Read line functions
 ***********************/
-inline void TraceBodyIO_csv::readTraceInfo(  const std::string& line, MetadataManager& traceInfo ) const
+inline void TraceBodyIO_csv::readTraceInfo( const std::string& line, MetadataManager& traceInfo ) const
 {
   traceInfo.NewMetadata( line );
   // dummy set also to false if it is a comment
@@ -161,9 +162,9 @@ inline void TraceBodyIO_csv::readTraceInfo(  const std::string& line, MetadataMa
 inline void TraceBodyIO_csv::readEvents( const ResourceModel<>& whichResourceModel,
                                          const string& line,
                                          MemoryBlocks& records,
-                                         unordered_set<TEventType>& events,
+                                         unordered_set< TEventType >& events,
                                          TRecordTime& endTime ) const
-{ 
+{
   TCPUOrder CPU = 0;
   TApplOrder appl;
   TTaskOrder task;
@@ -176,19 +177,17 @@ inline void TraceBodyIO_csv::readEvents( const ResourceModel<>& whichResourceMod
   strLine.clear();
   strLine.str( line );
   // Read the common info
-  if ( !readCommon( whichResourceModel,
-                    strLine, CPU, appl, task, thread, 
-                    begintime, time, eventvalue, decimals ) )
+  if( !readCommon( whichResourceModel, strLine, CPU, appl, task, thread, begintime, time, eventvalue, decimals ) )
   {
     cerr << "Error reading state record (error in Common)." << endl;
     cerr << line << endl;
     return;
   }
-  if ( endTime < begintime + time )
+  if( endTime < begintime + time )
     endTime = begintime + time;
   TEventType eventtype = 1;
 
-  //Event
+  // Event
   records.newRecord();
   records.setRecordType( EVENT );
   records.setTime( begintime );
@@ -212,59 +211,59 @@ inline bool TraceBodyIO_csv::readCommon( const ResourceModel<>& whichResourceMod
                                          TRecordTime& time,
                                          TEventValue& eventvalue,
                                          double& decimals ) const
-{ 
+{
   static ProcessModel<>::ThreadLocation lastATT = { 0, 0, 0 };
 
-  std::getline( line, tmpstring, '.' ); 
+  std::getline( line, tmpstring, '.' );
 #ifdef USE_ATOLL
   appl = atoll( tmpstring.c_str() );
 #else
-  #ifdef USE_PRV_ATOLL
-  if( !prv_atoll<TApplOrder>( tmpstring.c_str(), &appl ) )
-  #else
+#  ifdef USE_PRV_ATOLL
+  if( !prv_atoll< TApplOrder >( tmpstring.c_str(), &appl ) )
+#  else
   fieldStream.clear();
   fieldStream.str( tmpstring );
-  if ( !( fieldStream >> appl ) )
-  #endif
+  if( !( fieldStream >> appl ) )
+#  endif
   {
     return false;
   }
 #endif
 
-  std::getline( line, tmpstring, '.' ); 
+  std::getline( line, tmpstring, '.' );
 #ifdef USE_ATOLL
   task = atoll( tmpstring.c_str() );
 #else
-  #ifdef USE_PRV_ATOLL
-  if( !prv_atoll<TTaskOrder>( tmpstring.c_str(), &task ) )
-  #else
+#  ifdef USE_PRV_ATOLL
+  if( !prv_atoll< TTaskOrder >( tmpstring.c_str(), &task ) )
+#  else
   fieldStream.clear();
   fieldStream.str( tmpstring );
-  if ( !( fieldStream >> task ) )
-  #endif
+  if( !( fieldStream >> task ) )
+#  endif
   {
     return false;
   }
 #endif
 
-  std::getline( line, tmpstring, '\t' );  
+  std::getline( line, tmpstring, '\t' );
 #ifdef USE_ATOLL
   thread = atoll( tmpstring.c_str() );
 #else
-  #ifdef USE_PRV_ATOLL
-  if( !prv_atoll<TThreadOrder>( tmpstring.c_str(), &thread ) )
-  #else
+#  ifdef USE_PRV_ATOLL
+  if( !prv_atoll< TThreadOrder >( tmpstring.c_str(), &thread ) )
+#  else
   fieldStream.clear();
   fieldStream.str( tmpstring );
-  if ( !( fieldStream >> thread ) )
-  #endif
+  if( !( fieldStream >> thread ) )
+#  endif
   {
     return false;
   }
 #endif
 
   ProcessModel<>::ThreadLocation currATT = { appl, task, thread };
-  if ( !( currATT == lastATT ) )
+  if( !( currATT == lastATT ) )
   {
     lastATT = currATT;
     --currATT.appl;
@@ -274,17 +273,17 @@ inline bool TraceBodyIO_csv::readCommon( const ResourceModel<>& whichResourceMod
   }
 
 
-  std::getline( line, tmpstring, '.' ); 
+  std::getline( line, tmpstring, '.' );
 #ifdef USE_ATOLL
   begintime = atoll( tmpstring.c_str() );
 #else
-  #ifdef USE_PRV_ATOLL
-  if( !prv_atoll<TRecordTime>( tmpstring.c_str(), &begintime ) )
-  #else
+#  ifdef USE_PRV_ATOLL
+  if( !prv_atoll< TRecordTime >( tmpstring.c_str(), &begintime ) )
+#  else
   fieldStream.clear();
   fieldStream.str( tmpstring );
-  if ( !( fieldStream >> begintime ) )
-  #endif
+  if( !( fieldStream >> begintime ) )
+#  endif
   {
     return false;
   }
@@ -295,13 +294,13 @@ inline bool TraceBodyIO_csv::readCommon( const ResourceModel<>& whichResourceMod
 #ifdef USE_ATOLL
   time = atoll( tmpstring.c_str() );
 #else
-  #ifdef USE_PRV_ATOLL
-  if( !prv_atoll<TRecordTime>( tmpstring.c_str(), &time ) )
-  #else
+#  ifdef USE_PRV_ATOLL
+  if( !prv_atoll< TRecordTime >( tmpstring.c_str(), &time ) )
+#  else
   fieldStream.clear();
   fieldStream.str( tmpstring );
-  if ( !( fieldStream >> time ) )
-  #endif
+  if( !( fieldStream >> time ) )
+#  endif
   {
     return false;
   }
@@ -313,13 +312,13 @@ inline bool TraceBodyIO_csv::readCommon( const ResourceModel<>& whichResourceMod
 #ifdef USE_ATOLL
   eventvalue = atoll( tmpstring.c_str() );
 #else
-  #ifdef USE_PRV_ATOLL
-  if( !prv_atoll<TEventValue>( tmpstring.c_str(), &eventvalue, &decimals ) )
-  #else
+#  ifdef USE_PRV_ATOLL
+  if( !prv_atoll< TEventValue >( tmpstring.c_str(), &eventvalue, &decimals ) )
+#  else
   fieldStream.clear();
   fieldStream.str( tmpstring );
-  if ( !( fieldStream >> eventvalue ) )
-  #endif
+  if( !( fieldStream >> eventvalue ) )
+#  endif
   {
     return false;
   }
