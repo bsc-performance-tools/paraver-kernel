bf8a70b6d7de2f01f7d400bf7bf84e14140b8a09
Lluis Roca <lrocarom@gmail.com>
2025-10-14 12:17:46 +0200

    add new params to the clone function in histogram to avoid redrawing the cloned histogram
    (cherry picked from commit aa6761ad8aab381b5644075a5fd61c0bea471223)

--- src/khistogram.cpp
+++ src/khistogram.cpp
@@ -1776,6 +1776,8 @@ KHistogram* KHistogram::clone()
 {
   KHistogram* clonedKHistogram = new KHistogram();
 
+  clonedKHistogram->myTimeUnit = getTimeUnit();
+
   clonedKHistogram->controlWindow = nullptr;
   clonedKHistogram->dataWindow    = nullptr;
   if( clonedKHistogram->xtraControlWindow != nullptr )
